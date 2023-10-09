d413cd1a02346af48569e27428b0ba462da7a6b1
NPavlovaaa <n.pavlova.web@gmail.com>
2023-10-09 20:16:55 +0300

    Create Pagination component and hook for it

--- src/pages/MainPage/components/VacancyList/VacancyList.jsx
+++ src/pages/MainPage/components/VacancyList/VacancyList.jsx
@@ -1,8 +1,10 @@
 import ItemVacancyList from "../ItemVacancyList";
 import {useGetVacanciesQuery} from "../../../../api/apiSlice";
+import {useMemo, useState} from "react";
+import Pagination from "../../../../components/pagination/Pagination";
 
 const VacancyList = () => {
-
+    const [currentPage, setCurrentPage] = useState(1);
     const {
         data = [],
         isLoading,
@@ -11,19 +13,31 @@ const VacancyList = () => {
 
     const vacancyList = data.vacancies?.vacancy;
 
+    let pageSize = 5;
+    const currentData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * pageSize;
+        const lastPageIndex = firstPageIndex + pageSize;
+        return vacancyList?.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage, vacancyList]);
 
     return(
-        <div className="container">
-
+        <div className="container py-5">
             <div className="row">
-                {vacancyList ? vacancyList.map(item => {
+                {currentData?.map(item => {
                     return (
                         <div className="col-12" key={item.url}>
                             <ItemVacancyList item={item}/>
                         </div>
                     )
-                }) :  null}
+                })}
             </div>
+            <Pagination
+                className="mt-5 justify-content-center"
+                currentPage={currentPage}
+                totalCount={vacancyList ? vacancyList?.length : 0}
+                pageSize={pageSize}
+                onPageChange={page => setCurrentPage(page)}
+            />
         </div>
 
     )
